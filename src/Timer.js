49521a2fb174685def962f7a105216c7d7893829
A.J. Romaniello <ajiellodev@gmail.com>
2021-05-31 11:21:01 -0600

    commit tree d55fc03e1e629fcb8109ab9ad632168594813a9f

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
-class Timer extends Component {
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -11,6 +11,13 @@ class Timer extends Component {
   }
 
   //Your code here
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return this.state.time !== nextState.time
+  }
 
   componentDidMount() {
     this.interval = setInterval(
